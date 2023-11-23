0b7cfe4e7e397d5af55b7409b4e90e0128806c0d
LMH <vpsxja3268@naver.com>
2023-11-23 15:11:38 +0900

    prob4

---
+++ chap9/prob4/main.c
@@ -0,0 +1,21 @@
+#include <sys/wait.h>
+#include <stdio.h>
+
+int main(){
+	int status;
+
+	if((status = system("date")) < 0){
+		perror("system() error");
+	}
+	printf("end code %d\n", WEXITSTATUS(status));
+
+	if((status = system("hello")) < 0){
+                perror("system() error");
+        }
+        printf("end code %d\n", WEXITSTATUS(status));
+
+	if((status = system("who; exit 44")) < 0){
+                perror("system() error");
+        }
+        printf("end code %d\n", WEXITSTATUS(status));
+}
