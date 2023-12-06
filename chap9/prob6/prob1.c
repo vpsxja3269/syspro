546faa01c87b78f6f949f5c928d0b35139efc78e
LMH <vpsxja3268@cu.ac.kr>
2023-12-06 23:06:45 +0900

    prob6

--- chap9/prob6/prob1.c
+++ chap9/prob6/prob1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-main(){
-	int pid, gid; 
+main()
+{
+    int pid, gid;
+    printf("PARENT: PID = %d GID = %d\n", getpid(), getpgrp());
+    pid = fork();
+    if (pid == 0) {  // 자식 프로세스
+        printf("CHILD: PID = %d GID = %d\n", getpid(), getpgrp());
+    }
 }
