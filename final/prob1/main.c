06a36832a4ca3e31f372a0d9f41fc19519427943
LMH <vpsxja3268@cu.ac.kr>
2023-12-14 14:16:43 +0900

    d

---
+++ final/prob1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int type_num;
+	scanf("type num of childs :");
+	
+	/*for(int i = 0 ; i < type_num ; i++){
+		pid[i] = fork();
+		printf("[child %d] : Started! pid = %d, sleep=%d", i, getpid(), i+1);
+		sleep(i+1);
+	}
+	*/
+}
