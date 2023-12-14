004b41363bf1bbbddd640e35016950870507e62a
LMH <vpsxja3268@cu.ac.kr>
2023-12-14 15:01:30 +0900

    d

--- final/prob1/main.c
+++ final/prob1/main.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 
 int main(){
-	int type_num, pid[100];
+	int type_num, pid[100], i = 0;
 	
 	printf("type num of childs : ");
 	scanf("%d", &type_num);
 	
-	for(int i = 0 ; i < type_num ; i++){
+	for( i ; i < type_num ; i++){
 		pid[i] = fork();
+		if(pid[i] == 0){
 			printf("[child %d] : Started! pid = %d, sleep=%d\n", i, getpid(), i+1);
-			sleep(i+1);
+			exit(0);
+		}
+		sleep(i+1);
 	}
-
-	printf("parrent killed - last child id = %d\n", getpid());
+	
+	printf("parrent killed - last child id = %d\n", getpid(i));
 }
